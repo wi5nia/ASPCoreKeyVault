4f70f70aaf19a5cf2ab351cf96d08e9747c4c3c4
Tomasz Wiśniewski <tomaszwi@microsoft.com>
2018-07-12 16:44:03 +0200

    Initial commit

--- ASPCoreKeyVault/Pages/About.cshtml.cs
+++ ASPCoreKeyVault/Pages/About.cshtml.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ASPCoreKeyVault.Pages
 {
     public class AboutModel : PageModel
     {
+        public AboutModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private readonly IConfiguration _configuration = null;
         public string Message { get; set; }
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "My key val = " + _configuration["AppSecret"];
         }
     }
 }
--- ASPCoreKeyVault/Program.cs
+++ ASPCoreKeyVault/Program.cs
@@ -1,25 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+
 
 namespace ASPCoreKeyVault
 {
     public class Program
     {
+        private static string GetKeyVaultEndpoint() => "https://xkomkv.vault.azure.net";
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((ctx, builder) =>
+            {
+                var keyVaultEndpoint = GetKeyVaultEndpoint();
+                if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                {
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+                    builder.AddAzureKeyVault(
+                        keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                }
+            }
+         )
+            .UseStartup<Startup>()
+            .Build();
     }
 }
